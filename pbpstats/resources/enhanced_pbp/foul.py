609688a1a3bb0f8f7353ce8516a1091175c29f46
Darryl Blackport <darryl@Darryls-MacBook-Pro.local>
2020-04-19 13:41:25 -0400

    add event stats for fouls

--- pbpstats/resources/enhanced_pbp/foul.py
+++ pbpstats/resources/enhanced_pbp/foul.py
@@ -1,3 +1,6 @@
+import pbpstats
+
+
 class Foul(object):
     event_type = 6
 
@@ -70,5 +73,57 @@ class Foul(object):
         return self.event_action_type == 29
 
     @property
+    def foul_type_string(self):
+        if self.is_personal_foul:
+            return pbpstats.PERSONAL_FOUL_TYPE_STRING
+        if self.is_shooting_foul:
+            return pbpstats.SHOOTING_FOUL_TYPE_STRING
+        if self.is_loose_ball_foul:
+            return pbpstats.LOOSE_BALL_FOUL_TYPE_STRING
+        if self.is_offensive_foul:
+            return pbpstats.OFFENSIVE_FOUL_TYPE_STRING
+        if self.is_inbound_foul:
+            return pbpstats.INBOUND_FOUL_TYPE_STRING
+        if self.is_away_from_play_foul:
+            return pbpstats.AWAY_FROM_PLAY_FOUL_TYPE_STRING
+        if self.is_clear_path_foul:
+            return pbpstats.CLEAR_PATH_FOUL_TYPE_STRING
+        if self.is_double_foul:
+            return pbpstats.DOUBLE_FOUL_TYPE_STRING
+        if self.event_action_type == 14:
+            return pbpstats.FLAGRANT_1_FOUL_TYPE_STRING
+        if self.event_action_type == 15:
+            return pbpstats.FLAGRANT_2_FOUL_TYPE_STRING
+        if self.is_defensive_3_seconds:
+            return pbpstats.DEFENSIVE_3_SECONDS_FOUL_TYPE_STRING
+        if self.is_charge:
+            return pbpstats.CHARGE_FOUL_TYPE_STRING
+        if self.is_personal_block_foul:
+            return pbpstats.PERSONAL_BLOCK_TYPE_STRING
+        if self.is_personal_take_foul:
+            return pbpstats.PERSONAL_TAKE_TYPE_STRING
+        if self.is_shooting_block_foul:
+            return pbpstats.SHOOTING_BLOCK_TYPE_STRING
+
+    @property
     def event_stats(self):
-        return self.base_stats
+        stats = []
+        foul_type = self.foul_type_string
+        if foul_type is not None:
+            stats.append({'player_id': self.player1_id, 'team_id': self.team_id, 'stat_key': foul_type, 'stat_value': 1})
+            if hasattr(self, 'player3_id'):
+                p3_stat_key = foul_type if self.is_double_foul else foul_type + pbpstats.FOULS_DRAWN_TYPE_STRING
+                team_ids = list(self.current_players.keys())
+                opponent_team_id = team_ids[0] if self.team_id == team_ids[1] else team_ids[1]
+                if self.player3_id in self.current_players[self.team_id]:
+                    stats.append({'player_id': self.player3_id, 'team_id': self.team_id, 'stat_key': p3_stat_key, 'stat_value': 1})
+                elif self.player3_id in self.current_players[opponent_team_id]:
+                    stats.append({'player_id': self.player3_id, 'team_id': opponent_team_id, 'stat_key': p3_stat_key, 'stat_value': 1})
+
+                lineups_ids = self.lineup_ids
+                for stat in stats:
+                    opponent_team_id = team_ids[0] if stat['team_id'] == team_ids[1] else team_ids[1]
+                    stat['lineup_id'] = lineups_ids[stat['team_id']]
+                    stat['opponent_team_id'] = opponent_team_id
+                    stat['opponent_lineup_id'] = lineups_ids[opponent_team_id]
+        return self.base_stats + stats
