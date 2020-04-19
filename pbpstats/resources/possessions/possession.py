8034cded9a3ea8fcd255150aa9f0a172242fbb9d
Darryl Blackport <darryl@Darryls-MacBook-Pro.local>
2020-04-19 18:36:16 -0400

    add possession_stats property

--- pbpstats/resources/possessions/possession.py
+++ pbpstats/resources/possessions/possession.py
@@ -1,9 +1,12 @@
+from itertools import groupby
+from operator import itemgetter
+
+
 class Possession(object):
     def __init__(self, events):
         self.game_id = events[0].game_id
         self.period = events[0].period
         self.events = events
-        self.stats = [event.event_stats for event in events]
 
     def __repr__(self):
         return (
@@ -29,3 +32,15 @@ class Possession(object):
     @property
     def offense_team_id(self):
         return self.events[0].get_offense_team_id()
+
+    @property
+    def possession_stats(self):
+        grouper = itemgetter('player_id', 'team_id', 'opponent_team_id', 'lineup_id', 'opponent_lineup_id', 'stat_key')
+        results = []
+        event_stats = [event_stat for event in self.events for event_stat in event.event_stats]
+        for key, group in groupby(sorted(event_stats, key=grouper), grouper):
+            temp_dict = dict(zip(['player_id', 'team_id', 'opponent_team_id', 'lineup_id', 'opponent_lineup_id', 'stat_key'], key))
+            temp_dict['stat_value'] = sum(item['stat_value'] for item in group)
+            results.append(temp_dict)
+
+        return results
