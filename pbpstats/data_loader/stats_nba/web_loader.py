b9df7a829fb3ff09b780cb0ef5c409350435dae2
Darryl Blackport <darryl@Darryls-MacBook-Pro.local>
2020-04-19 09:51:33 -0400

    clean up season property

--- pbpstats/data_loader/stats_nba/web_loader.py
+++ pbpstats/data_loader/stats_nba/web_loader.py
@@ -43,17 +43,17 @@ class StatsNbaWebLoader(AbsDataLoader, StatsNbaLoaderBase):
         ex. for 2016-17 season 4th and 5th characters would be 16
         for WNBA, season is just year, ex. 2016
         """
-        digit4 = self.game_id[3]
-        digit5 = self.game_id[4]
-        if digit4 == '9':
-            if digit5 == '9':
+        digit4 = int(self.game_id[3])
+        digit5 = int(self.game_id[4])
+        if digit4 == 9:
+            if digit5 == 9:
                 return '1999' if self.league == WNBA_STRING else '1999-00'
             else:
-                return '19' + digit4 + digit5 if self.league == WNBA_STRING else '19' + digit4 + digit5 + '-' + digit4 + str(int(digit5) + 1)
-        elif digit5 == '9':
-            return '20' + digit4 + digit5 if self.league == WNBA_STRING else '20' + digit4 + digit5 + '-' + str(int(digit4) + 1) + '0'
+                return f'19{digit4}{digit5}' if self.league == WNBA_STRING else f'19{digit4}{digit5}-{digit4}{digit5 + 1}'
+        elif digit5 == 9:
+            return f'20{digit4}{digit5}' if self.league == WNBA_STRING else f'20{digit4}{digit5}-{digit4 + 1}0'
         else:
-            return '20' + digit4 + digit5 if self.league == WNBA_STRING else '20' + digit4 + digit5 + '-' + digit4 + str(int(digit5) + 1)
+            return f'20{digit4}{digit5}'if self.league == WNBA_STRING else f'20{digit4}{digit5}-{digit4}{digit5 + 1}'
 
     @property
     def season_type(self):
