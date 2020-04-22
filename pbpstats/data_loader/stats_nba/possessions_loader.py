db1602d9d0ca3435b90cdf53ee054b546c9ca162
Darryl Blackport <darryl@Darryls-MacBook-Pro.local>
2020-04-22 19:10:24 -0400

    use bad pbp case overrides in possessions loader

--- pbpstats/data_loader/stats_nba/possessions_loader.py
+++ pbpstats/data_loader/stats_nba/possessions_loader.py
@@ -1,3 +1,7 @@
+import os
+import json
+
+from pbpstats.overrides import IntDecoder
 from pbpstats.data_loader.stats_nba.enhanced_pbp_loader import StatsNbaEnhancedPbpLoader
 from pbpstats.data_loader.nba_possession_loader import NbaPossessionLoader
 from pbpstats.resources.possessions.possession import Possession
@@ -13,11 +17,15 @@ class StatsNbaPossessionLoader(NbaPossessionLoader):
     parent_object = 'Game'
 
     def __init__(self, game_id, source, file_directory=None):
+
+        self.file_directory = file_directory
+        self.game_id = game_id
         pbp_events = StatsNbaEnhancedPbpLoader(game_id, source, file_directory)
         self.events = pbp_events.items
         events_by_possession = self._split_events_by_possession()
         self.items = [Possession(possession_events) for possession_events in events_by_possession]
         self._add_extra_attrs_to_all_possessions()
+        self._load_bad_possession_overrides()
         self._check_that_possessions_alternate()
 
     def _check_that_possessions_alternate(self):
@@ -28,19 +36,39 @@ class StatsNbaPossessionLoader(NbaPossessionLoader):
         for possession in self.items:
             if possession.previous_possession is not None:
                 if possession.offense_team_id == possession.previous_possession.offense_team_id:
-                    exception_text = (
-                        f'GameId: {possession.game_id}, Period: {possession.period}, '
-                        f'Number: {possession.number}, Events: {possession.events}, '
-                        f'Previous Events: {possession.previous_possession.events}>'
-                    )
+                    if not (
+                        self.game_id in self.bad_pbp_cases.keys() and
+                        possession.period in self.bad_pbp_cases[self.game_id].keys() and
+                        possession.number in self.bad_pbp_cases[self.game_id][possession.period]
+                    ):
+                        exception_text = (
+                            f'GameId: {possession.game_id}, Period: {possession.period}, '
+                            f'Number: {possession.number}, Events: {possession.events}, '
+                            f'Previous Events: {possession.previous_possession.events}>'
+                        )
 
-                    raise TeamHasBackToBackPossessionsException(exception_text)
+                        raise TeamHasBackToBackPossessionsException(exception_text)
             if possession.next_possession is not None:
                 if possession.offense_team_id == possession.next_possession.offense_team_id:
-                    exception_text = (
-                        f'GameId: {possession.game_id}, Period: {possession.period}, '
-                        f'Number: {possession.next_possession.number}, Events: {possession.next_possession.events}, '
-                        f'Previous Events: {possession.events}>'
-                    )
+                    if not (
+                        self.game_id in self.bad_pbp_cases.keys() and
+                        possession.period in self.bad_pbp_cases[self.game_id].keys() and
+                        possession.next_possession.number in self.bad_pbp_cases[self.game_id][possession.period]
+                    ):
+                        exception_text = (
+                            f'GameId: {possession.game_id}, Period: {possession.period}, '
+                            f'Number: {possession.next_possession.number}, Events: {possession.next_possession.events}, '
+                            f'Previous Events: {possession.events}>'
+                        )
 
-                    raise TeamHasBackToBackPossessionsException(exception_text)
+                        raise TeamHasBackToBackPossessionsException(exception_text)
+
+    def _load_bad_possession_overrides(self):
+        self.bad_pbp_cases = {}
+        if self.file_directory is not None:
+            bad_pbp_possessions_file_path = f'{self.file_directory}/overrides/bad_pbp_possessions.json'
+            if os.path.isfile(bad_pbp_possessions_file_path):
+                with open(bad_pbp_possessions_file_path) as f:
+                    # bad pbp where event is missing in pbp causing back to back possessions for same team - this will prevent back to back possession exception from being raised
+                    # {GameId: {Period:[EventNum]}}
+                    self.bad_pbp_cases = json.loads(f.read(), cls=IntDecoder)
--- pbpstats/overrides.py
+++ pbpstats/overrides.py
@@ -1,7 +1,4 @@
 import json
-import os
-
-from pbpstats import DATA_DIRECTORY
 
 
 class IntDecoder(json.JSONDecoder):
@@ -31,16 +28,3 @@ def convert_to_int_but_keep_game_id_string(value):
             return value
     except ValueError:
         return value
-
-
-if DATA_DIRECTORY is not None:
-    bad_pbp_possessions_file_path = f'{DATA_DIRECTORY}overrides/bad_pbp_possessions.json'
-    if os.path.isfile(bad_pbp_possessions_file_path):
-        with open(bad_pbp_possessions_file_path) as f:
-            # bad pbp where event is missing in pbp causing back to back possessions for same team - this will prevent back to back possession exception from being raised
-            # {GameId: {Period:[EventNum]}}
-            BAD_PBP_CASES = json.loads(f.read(), cls=IntDecoder)
-    else:
-        BAD_PBP_CASES = {}
-else:
-    BAD_PBP_CASES = {}
