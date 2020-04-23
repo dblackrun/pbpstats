ecde9f586ba96254e8cd242f284b9c43a23e52a5
Darryl Blackport <darryl@Darryls-MacBook-Pro.local>
2020-04-22 20:28:04 -0400

    add test for foul turnover on jump ball

--- tests/resources/test_jump_ball.py
+++ tests/resources/test_jump_ball.py
@@ -2,6 +2,7 @@ from pbpstats.resources.enhanced_pbp.stats_nba.jump_ball import StatsJumpBall
 from pbpstats.resources.enhanced_pbp.stats_nba.field_goal import StatsFieldGoal
 from pbpstats.resources.enhanced_pbp.stats_nba.rebound import StatsRebound
 from pbpstats.resources.enhanced_pbp.stats_nba.turnover import StatsTurnover
+from pbpstats.resources.enhanced_pbp.stats_nba.foul import StatsFoul
 
 
 def test_dangling_jump_ball_changes_possession():
@@ -91,6 +92,40 @@ def test_jump_ball_turnover_previous_event_not_possession_change():
     jump_ball_event.next_event = shot2_event
     shot2_event.previous_event = jump_ball_event
     shot2_event.next_event = None
+
+    jump_ball_event.possession_changing_override = False
+    jump_ball_event.non_possession_changing_override = False
+
+    assert jump_ball_event.is_possession_ending_event is False
+
+
+def test_jump_ball_foul_turnover_next_event_not_possession_change():
+    shot = {'GAME_ID': '0021900510', 'EVENTNUM': 225, 'PCTIMESTRING': '6:40', 'VISITORDESCRIPTION': 'MISS Lowry  3PT Jump Shot', 'EVENTMSGACTIONTYPE': 1, 'EVENTMSGTYPE': 2, 'PLAYER1_ID': 200768, 'PLAYER1_TEAM_ID': 1610612761}
+    order = 1
+    shot_event = StatsFieldGoal(shot, order)
+    rebound = {'GAME_ID': '0021900510', 'EVENTNUM': 226, 'PCTIMESTRING': '6:38', 'HOMEDESCRIPTION': 'Silva REBOUND (Off:0 Def:1)', 'EVENTMSGACTIONTYPE': 0, 'EVENTMSGTYPE': 4, 'PLAYER1_ID': 1629735, 'PLAYER1_TEAM_ID': 1610612748}
+    order = 1
+    rebound_event = StatsRebound(rebound, order)
+    jump_ball = {'GAME_ID': '0021900510', 'EVENTNUM': 228, 'PCTIMESTRING': '6:29', 'HOMEDESCRIPTION': 'Jump Ball Silva vs. Boucher: Tip to Johnson', 'EVENTMSGACTIONTYPE': 0, 'EVENTMSGTYPE': 10, 'PLAYER1_ID': 1629735, 'PLAYER1_TEAM_ID': 1610612748, 'PLAYER2_ID': 1628449, 'PLAYER2_TEAM_ID': 1610612761, 'PLAYER3_ID': 1626169, 'PLAYER3_TEAM_ID': 1610612761}
+    order = 1
+    jump_ball_event = StatsJumpBall(jump_ball, order)
+    foul = {'GAME_ID': '0021900510', 'EVENTNUM': 233, 'PCTIMESTRING': '6:29', 'HOMEDESCRIPTION': "Silva P.FOUL", 'EVENTMSGACTIONTYPE': 1, 'EVENTMSGTYPE': 6, 'PLAYER1_ID': 1629735, 'PLAYER1_TEAM_ID': 1610612748, 'PLAYER2_ID': 1628449, 'PLAYER2_TEAM_ID': 1610612761}
+    order = 1
+    foul_event = StatsFoul(foul, order)
+    turnover = {'GAME_ID': '0021900510', 'EVENTNUM': 233, 'PCTIMESTRING': '6:29', 'HOMEDESCRIPTION': "Silva Foul Turnover", 'EVENTMSGACTIONTYPE': 37, 'EVENTMSGTYPE': 5, 'PLAYER1_ID': 1629735, 'PLAYER1_TEAM_ID': 1610612748}
+    order = 1
+    turnover_event = StatsTurnover(turnover, order)
+
+    shot_event.previous_event = None
+    shot_event.next_event = rebound_event
+    rebound_event.previous_event = shot_event
+    rebound_event.next_event = jump_ball_event
+    jump_ball_event.previous_event = rebound_event
+    jump_ball_event.next_event = foul_event
+    foul_event.previous_event = jump_ball_event
+    foul_event.next_event = turnover_event
+    turnover_event.previous_event = foul_event
+    turnover_event.next_event = None
 
     jump_ball_event.possession_changing_override = False
     jump_ball_event.non_possession_changing_override = False
