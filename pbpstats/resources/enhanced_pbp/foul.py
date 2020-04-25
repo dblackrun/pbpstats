69c626fa517f2b20fe40516d1593e2a5d8afa9c0
Darryl Blackport <darryl@Darryls-MacBook-Pro.local>
2020-04-24 20:54:45 -0400

    fix indent level of where opponent and lineups get added

--- pbpstats/resources/enhanced_pbp/foul.py
+++ pbpstats/resources/enhanced_pbp/foul.py
@@ -119,19 +119,19 @@ class Foul(object):
         foul_type = self.foul_type_string
         if foul_type is not None:
             stats.append({'player_id': self.player1_id, 'team_id': self.team_id, 'stat_key': foul_type, 'stat_value': 1})
+            team_ids = list(self.current_players.keys())
             if hasattr(self, 'player3_id'):
                 p3_stat_key = foul_type if self.is_double_foul else foul_type + pbpstats.FOULS_DRAWN_TYPE_STRING
-                team_ids = list(self.current_players.keys())
                 opponent_team_id = team_ids[0] if self.team_id == team_ids[1] else team_ids[1]
                 if self.player3_id in self.current_players[self.team_id]:
                     stats.append({'player_id': self.player3_id, 'team_id': self.team_id, 'stat_key': p3_stat_key, 'stat_value': 1})
                 elif self.player3_id in self.current_players[opponent_team_id]:
                     stats.append({'player_id': self.player3_id, 'team_id': opponent_team_id, 'stat_key': p3_stat_key, 'stat_value': 1})
 
-                lineups_ids = self.lineup_ids
-                for stat in stats:
-                    opponent_team_id = team_ids[0] if stat['team_id'] == team_ids[1] else team_ids[1]
-                    stat['lineup_id'] = lineups_ids[stat['team_id']]
-                    stat['opponent_team_id'] = opponent_team_id
-                    stat['opponent_lineup_id'] = lineups_ids[opponent_team_id]
+            lineups_ids = self.lineup_ids
+            for stat in stats:
+                opponent_team_id = team_ids[0] if stat['team_id'] == team_ids[1] else team_ids[1]
+                stat['lineup_id'] = lineups_ids[stat['team_id']]
+                stat['opponent_team_id'] = opponent_team_id
+                stat['opponent_lineup_id'] = lineups_ids[opponent_team_id]
         return self.base_stats + stats
