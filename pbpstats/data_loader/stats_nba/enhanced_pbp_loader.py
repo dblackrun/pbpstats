bd4ea815291f17aa9ee226e0cbd3065a74c057ef
Darryl Blackport <darryl@Darryls-MacBook-Pro.local>
2020-04-25 14:57:47 -0400

    fix event order on common out of order cases

--- pbpstats/data_loader/stats_nba/enhanced_pbp_loader.py
+++ pbpstats/data_loader/stats_nba/enhanced_pbp_loader.py
@@ -3,6 +3,7 @@ from pbpstats.data_loader.stats_nba.shots_loader import StatsNbaShotsLoader
 from pbpstats.data_loader.nba_enhanced_pbp_loader import NbaEnhancedPbpLoader
 from pbpstats.resources.enhanced_pbp.stats_nba.enhanced_pbp_factory import StatsNbaEnhancedPbpFactory
 from pbpstats.resources.enhanced_pbp.field_goal import FieldGoal
+from pbpstats.resources.enhanced_pbp.rebound import EventOrderError
 
 
 class StatsNbaEnhancedPbpLoader(StatsNbaPbpLoader, NbaEnhancedPbpLoader):
@@ -14,10 +15,11 @@ class StatsNbaEnhancedPbpLoader(StatsNbaPbpLoader, NbaEnhancedPbpLoader):
         super().__init__(game_id, source, file_directory)
 
     def _make_pbp_items(self):
-        factory = StatsNbaEnhancedPbpFactory()
-        self.items = [factory.get_event_class(item['EVENTMSGTYPE'])(item, i) for i, item in enumerate(self.data)]
-        self._add_shot_x_y_coords()
+        self.factory = StatsNbaEnhancedPbpFactory()
+        self.items = [self.factory.get_event_class(item['EVENTMSGTYPE'])(item, i) for i, item in enumerate(self.data)]
         self._add_extra_attrs_to_all_events()
+        self._check_rebound_event_order(6)
+        self._add_shot_x_y_coords()
 
     def _add_shot_x_y_coords(self):
         shots_loader = StatsNbaShotsLoader(self.game_id, self.source, self.file_directory)
@@ -26,3 +28,97 @@ class StatsNbaEnhancedPbpLoader(StatsNbaPbpLoader, NbaEnhancedPbpLoader):
             if isinstance(item, FieldGoal) and item.event_num in shots_event_num_map.keys():
                 item.locX = shots_event_num_map[item.event_num]['loc_x']
                 item.locY = shots_event_num_map[item.event_num]['loc_y']
+
+    def _check_rebound_event_order(self, max_retries):
+        """
+        checks rebound events to make sure they are ordered correctly
+        """
+        attempts = 0
+        while attempts <= max_retries:
+            try:
+                for event in self.items:
+                    if hasattr(event, 'missed_shot'):
+                        event.missed_shot
+                break
+            except EventOrderError as e:
+                self._fix_common_event_order_error(e)
+                self.items = [self.factory.get_event_class(item['EVENTMSGTYPE'])(item, i) for i, item in enumerate(self.data)]
+                self._add_extra_attrs_to_all_events()
+                attempts += 1
+
+    def _fix_common_event_order_error(self, exception):
+        """
+        fixs common cases where events are out of order
+        current cases are:
+        - subs/timeouts between free throws being between missed second FT and rebound
+        - end of period replay events being between missed shot and rebound
+        - rebound and shot in reverse order
+        - shot, rebound, rebound - first rebound needs to me moved to before shot
+        - shot, rebound, rebound - second rebound needs to be moved ahead of shot and first rebound
+        """
+        event_num = int(str(exception).split('EventNum: ')[-1].split('>')[0])
+        headers = self.source_data['resultSets'][0]['headers']
+        rows = self.source_data['resultSets'][0]['rowSet']
+        event_num_index = headers.index('EVENTNUM')
+        event_type_index = headers.index('EVENTMSGTYPE')
+        for i, row in enumerate(rows):
+            if row[event_num_index] == int(event_num):
+                issue_event_index = i
+
+        if rows[issue_event_index][event_type_index] in [8, 9]:
+            # these are subs/timeouts that are in between missed ft and rebound
+            # move all sub/timeout events between ft and rebound to before ft
+            row_index = issue_event_index
+            while rows[row_index][event_type_index] in [8, 9]:
+                row_index -= 1
+
+            # rows[row_index] should be moved to right before rows[issue_event_index]
+            new_rows = []
+            row_to_move_event_num = rows[row_index][event_num_index]
+            for row in rows:
+                if row[event_num_index] == row_to_move_event_num:
+                    row_to_move = row
+                elif row[event_num_index] == int(event_num):
+                    new_rows.append(row)
+                    new_rows.append(row_to_move)
+                else:
+                    new_rows.append(row)
+
+            self.source_data['resultSets'][0]['rowSet'] = new_rows
+        elif rows[issue_event_index][event_type_index] == 18 and rows[issue_event_index + 1][event_type_index] == 4:
+            # these are replays that are in between missed shot and rebound
+            # move replay event after rebound
+            replay_event = rows[issue_event_index]
+            rebound_event = rows[issue_event_index + 1]
+
+            self.source_data['resultSets'][0]['rowSet'][issue_event_index + 1] = replay_event
+            self.source_data['resultSets'][0]['rowSet'][issue_event_index] = rebound_event
+
+        elif rows[issue_event_index + 1][event_type_index] == 4 and rows[issue_event_index + 1][event_num_index] == int(event_num) - 1:
+            # rebound and shot need to be flipped
+            rebound_event = rows[issue_event_index + 1]
+            shot_event = rows[issue_event_index]
+            self.source_data['resultSets'][0]['rowSet'][issue_event_index] = rebound_event
+            self.source_data['resultSets'][0]['rowSet'][issue_event_index + 1] = shot_event
+        elif (
+                rows[issue_event_index + 1][event_type_index] == 4 and rows[issue_event_index + 1][event_num_index] == int(event_num) + 2 and
+                rows[issue_event_index - 1][event_type_index] == 2 and rows[issue_event_index - 1][event_num_index] == int(event_num) + 1
+        ):
+            # shot, rebound, rebound - first rebound need to me moved to before shot
+            rebound_event = rows[issue_event_index]
+            shot_event = rows[issue_event_index - 1]
+            self.source_data['resultSets'][0]['rowSet'][issue_event_index] = shot_event
+            self.source_data['resultSets'][0]['rowSet'][issue_event_index - 1] = rebound_event
+        elif (
+                rows[issue_event_index + 1][event_type_index] == 4 and rows[issue_event_index + 1][event_num_index] == int(event_num) - 2 and
+                rows[issue_event_index - 1][event_type_index] == 2 and rows[issue_event_index - 1][event_num_index] == int(event_num) - 1
+        ):
+            # shot, rebound, rebound - second rebound needs to be moved ahead of shot and first rebound
+            first_rebound = rows[issue_event_index + 1]
+            second_rebound = rows[issue_event_index]
+            shot_event = rows[issue_event_index - 1]
+            self.source_data['resultSets'][0]['rowSet'][issue_event_index + 1] = second_rebound
+            self.source_data['resultSets'][0]['rowSet'][issue_event_index] = shot_event
+            self.source_data['resultSets'][0]['rowSet'][issue_event_index - 1] = first_rebound
+
+        self._save_data_to_file()
--- tests/data_loaders/test_stats_enhanced_pbp_loader.py
+++ tests/data_loaders/test_stats_enhanced_pbp_loader.py
@@ -162,3 +162,102 @@ class TestStatsEnhancedPbpLoader:
         assert str(pbp_loader.items[0].data['next_event']) == self.expected_first_item_data['next_event']
         assert pbp_loader.items[0].data['team_starting_with_ball'] == self.expected_first_item_data['team_starting_with_ball']
         assert pbp_loader.items[0].data['period_starters'] == self.expected_first_item_data['period_starters']
+
+    def test_web_loader_with_events_out_of_order_loads_data(self):
+        game_id = '0021900001'
+        with open(f'{self.data_directory}/pbp/raw_stats_{game_id}.json') as f:
+            pbp_response = json.loads(f.read())
+        base_url = 'https://stats.nba.com/stats/playbyplayv2'
+        query_params = {
+            'GameId': game_id,
+            'StartPeriod': 0,
+            'EndPeriod': 10,
+            'RangeType': 2,
+            'StartRange': 0,
+            'EndRange': 55800
+        }
+        pbp_url = furl(base_url).add(query_params).url
+        responses.add(responses.GET, pbp_url, json=pbp_response, status=200)
+
+        with open(f'{self.data_directory}/game_details/stats_home_shots_{game_id}.json') as f:
+            home_response = json.loads(f.read())
+        with open(f'{self.data_directory}/game_details/stats_away_shots_{game_id}.json') as f:
+            away_response = json.loads(f.read())
+        base_url = 'https://stats.nba.com/stats/shotchartdetail'
+        home_query_params = {
+            'GameID': game_id,
+            'TeamID': 1610612761,
+            'Season': '2016-17',
+            'SeasonType': 'Regular Season',
+            'PlayerID': 0,
+            'Outcome': '',
+            'Location': '',
+            'Month': 0,
+            'SeasonSegment': '',
+            'DateFrom': '',
+            'DateTo': '',
+            'OpponentTeamID': 0,
+            'VsConference': '',
+            'VsDivision': '',
+            'Position': '',
+            'RookieYear': '',
+            'GameSegment': '',
+            'Period': 0,
+            'LastNGames': 0,
+            'ContextMeasure': 'FG_PCT',
+            'PlayerPosition': '',
+            'LeagueID': '00',
+        }
+        away_query_params = {
+            'GameID': game_id,
+            'TeamID': 1610612740,
+            'Season': '2016-17',
+            'SeasonType': 'Regular Season',
+            'PlayerID': 0,
+            'Outcome': '',
+            'Location': '',
+            'Month': 0,
+            'SeasonSegment': '',
+            'DateFrom': '',
+            'DateTo': '',
+            'OpponentTeamID': 0,
+            'VsConference': '',
+            'VsDivision': '',
+            'Position': '',
+            'RookieYear': '',
+            'GameSegment': '',
+            'Period': 0,
+            'LastNGames': 0,
+            'ContextMeasure': 'FG_PCT',
+            'PlayerPosition': '',
+            'LeagueID': '00',
+        }
+        home_url = furl(base_url).add(home_query_params).url
+        away_url = furl(base_url).add(away_query_params).url
+        responses.add(responses.GET, home_url, json=home_response, status=200)
+        responses.add(responses.GET, away_url, json=away_response, status=200)
+
+        with open(f'{self.data_directory}/game_details/stats_summary_{game_id}.json') as f:
+            summary_response = json.loads(f.read())
+        summary_base_url = 'https://stats.nba.com/stats/boxscoresummaryv2'
+        query_params = {'GameId': self.game_id}
+        summary_url = furl(summary_base_url).add(query_params).url
+        responses.add(responses.GET, summary_url, json=summary_response, status=200)
+
+        with open(f'{self.data_directory}/period_starters_boxscore_response.json') as f:
+            starters_response = json.loads(f.read())
+        boxscore_base_url = 'https://stats.nba.com/stats/boxscoretraditionalv2'
+        starters_query_params = {
+            'GameId': self.game_id,
+            'StartPeriod': 0,
+            'EndPeriod': 0,
+            'RangeType': 2,
+            'StartRange': 28800,
+            'EndRange': 28940
+        }
+        starters_url = furl(boxscore_base_url).add(starters_query_params).url
+        responses.add(responses.GET, starters_url, json=starters_response, status=200)
+
+        file_directory = None
+        pbp_loader = StatsNbaEnhancedPbpLoader(game_id, 'web', file_directory)
+        assert len(pbp_loader.items) == 573
