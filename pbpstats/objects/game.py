514e880d78c8e74ac345ef9e2ef0a8779323d403
Darryl Blackport <darryl@darryl.darryl>
2020-05-08 13:36:39 -0400

    format code

--- pbpstats/objects/game.py
+++ pbpstats/objects/game.py
@@ -29,14 +29,25 @@ class Game(object):
 
     :param str game_id: NBA Stats Game Id
     """
+
     def __init__(self, game_id):
         self.game_id = game_id
-        attributes = inspect.getmembers(self, lambda a: not(inspect.isroutine(a)))
-        data_loaders = [a for a in attributes if a[0].endswith(client.DATA_LOADER_SUFFIX)]
-        data_source_map = {a[0].replace(client.DATA_SOURCE_SUFFIX, ''): a[1] for a in attributes if a[0].endswith(client.DATA_SOURCE_SUFFIX)}
+        attributes = inspect.getmembers(self, lambda a: not (inspect.isroutine(a)))
+        data_loaders = [
+            a for a in attributes if a[0].endswith(client.DATA_LOADER_SUFFIX)
+        ]
+        data_source_map = {
+            a[0].replace(client.DATA_SOURCE_SUFFIX, ""): a[1]
+            for a in attributes
+            if a[0].endswith(client.DATA_SOURCE_SUFFIX)
+        }
         for data_loader in data_loaders:
-            attr_name = data_loader[0].replace(client.DATA_LOADER_SUFFIX, '')
+            attr_name = data_loader[0].replace(client.DATA_LOADER_SUFFIX, "")
             source = data_source_map[attr_name]
             data = data_loader[1](game_id, source, self.data_directory)
             resource_cls = getattr(self, attr_name)
-            setattr(self, client.PATTERN.sub('_', attr_name).lower(), resource_cls(data.items))
+            setattr(
+                self,
+                client.PATTERN.sub("_", attr_name).lower(),
+                resource_cls(data.items),
+            )
