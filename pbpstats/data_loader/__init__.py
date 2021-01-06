fbfbea2ce31239cb5d185b2bcc3e3e48764e7f4c
Darryl Blackport <darryl@darryl.darryl>
2021-01-06 12:19:36 -0500

    add live boxscore endpoint

--- pbpstats/data_loader/__init__.py
+++ pbpstats/data_loader/__init__.py
@@ -3,6 +3,7 @@ from pbpstats.data_loader.data_nba.enhanced_pbp_loader import DataNbaEnhancedPbp
 from pbpstats.data_loader.data_nba.pbp_loader import DataNbaPbpLoader
 from pbpstats.data_loader.data_nba.possessions_loader import DataNbaPossessionLoader
 from pbpstats.data_loader.data_nba.schedule_loader import DataNbaScheduleLoader
+from pbpstats.data_loader.live.boxscore_loader import LiveBoxscoreLoader
 from pbpstats.data_loader.live.enhanced_pbp_loader import LiveEnhancedPbpLoader
 from pbpstats.data_loader.live.pbp_loader import LivePbpLoader
 from pbpstats.data_loader.live.possessions_loader import LivePossessionLoader
@@ -20,6 +21,7 @@ __all__ = [
     "DataNbaPbpLoader",
     "DataNbaPossessionLoader",
     "DataNbaScheduleLoader",
+    "LiveBoxscoreLoader",
     "LiveEnhancedPbpLoader",
     "LivePbpLoader",
     "LivePossessionLoader",
---
+++ pbpstats/resources/boxscore/live_boxscore_item.py
@@ -0,0 +1,30 @@
+OVERRIDES = {"personId": "player_id"}
+
+
+class LiveBoxscoreItem(object):
+    """
+    Class for boxscore items from live data endpoint
+
+    :param dict item: dict with boxscore stats from response
+    :param int team_id: (optional) team id is not in dict with stats and can be added in here
+    :param str team_abbreviation: (optional) team abbreviation is not in dict with stats and can be added in here
+    """
+
+    def __init__(self, item, team_id=None, team_abbreviation=None):
+        self.team_id = team_id
+        self.team_abbreviation = team_abbreviation
+        for key, value in item.items():
+            if key == "statistics":
+                for stat_key, stat_value in value.items():
+                    setattr(self, stat_key, stat_value)
+            else:
+                setattr(self, OVERRIDES.get(key, key), value)
+        if hasattr(self, "firstName") and hasattr(self, "familyName"):
+            self.name = f"{self.firstName} {self.familyName}"
+
+    @property
+    def data(self):
+        """
+        returns boxscore data dict
+        """
+        return self.__dict__
